cfb9d2d380891db2101f79a1ba55180fb576c5af
jhsz72 <jhsz72@mail.umkc.edu>
2017-02-22 14:13:15 -0600

    Added constructor

--- Code/Project1B/Poly.h
+++ Code/Project1B/Poly.h
@@ -9,6 +9,7 @@ private:
 	long coefficient; //Holds the multiplier
 	long exponent; //Holds the exponent
 public:
+	term(int coe, int exp) : coefficient(coe), exponent(exp) {}; //initialization
 	bool operator>(const term &rhs) { return exponent > rhs.exponent; } //Greater operator overload. Used to compare exponents
 	bool operator=(const term &rhs) { return exponent = rhs.exponent; } //Greater operator overload. Used to compare exponents
 	bool operator<(const term &rhs) { return exponent < rhs.exponent; } //Greater operator overload. Used to compare exponents
